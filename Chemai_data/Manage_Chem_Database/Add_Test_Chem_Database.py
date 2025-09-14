f3b19e8be232fe465fe27f2209b908659174326f
沽月 <2583526329@qq.com>
2025-09-14 18:05:45 +0800

    update

--- Chemai_data/Manage_Chem_Database/Add_Test_Chem_Database.py
+++ Chemai_data/Manage_Chem_Database/Add_Test_Chem_Database.py
@@ -4,7 +4,7 @@ from Chemai_data.models import Test_Chem_Database #Test_Chem_Database.Test_Chem_
 
 # 数据库前端添加操作
 def Add_Test(request):
-    test1 = Test_Chem_Database.Test_Chem_mol(Inchikey=request.GET['关键字'], Normal_boiling_temperature_Tb=request.GET['正常沸点温度'],
+    test1 = Test_Chem_Database.Test_Chem_mol(InchIkey=request.GET['关键字'], Normal_boiling_temperature_Tb=request.GET['正常沸点温度'],
                        Normal_melting_temperature_Tm=request.GET['正常熔化温度'],)
     test1.save()
     return HttpResponse("<p>数据添加成功！</p>")
