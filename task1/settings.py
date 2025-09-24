8d4b8a48a0806eb35663691a9582ef58f303de12
沽月 <2583526329@qq.com>
2025-09-25 00:34:17 +0800

    设置 ALLOWED_HOSTS = ["*"]

--- task1/settings.py
+++ task1/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure--1$yw4_i*v#i(pkdau#-=5a3a#btozu#v$t(zlrm=s!&^=o^sz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
